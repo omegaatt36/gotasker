6f5b9b0df5e3b0d920c441596bc8b4fc8f41a8de
Raiven Kao <omegaatt36@gmail.com>
2024-04-07 10:44:45 +0800

    feat: add domain model.

---
+++ domain/task.go
@@ -0,0 +1,35 @@
+//go:generate go-enum -f=$GOFILE
+
+package domain
+
+import "context"
+
+// Task represents a task.
+type Task struct {
+	ID     uint
+	Name   string
+	Status TaskStatus
+}
+
+// TaskStatus represents a task status.
+// ENUM(incomplete, completed)
+type TaskStatus int
+
+// TaskRepository represents a task repository.
+type TaskRepository interface {
+	CreateTask(ctx context.Context, req CreateTaskRequest) error
+	ListTasks(ctx context.Context) ([]Task, error)
+	UpdateTask(ctx context.Context, id uint, req UpdateTaskRequest) error
+	DeleteTask(ctx context.Context, id uint) error
+}
+
+// CreateTaskRequest defines the request for creating a task.
+type CreateTaskRequest struct {
+	Name string
+}
+
+// UpdateTaskRequest defines the request for updating a task.
+type UpdateTaskRequest struct {
+	Name   *string
+	Status *TaskStatus
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/omegaatt36/gotasker
+
+go 1.22.2
